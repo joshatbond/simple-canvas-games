aec993420f02ea203bb93ed413b72a65fd2ad899
Joshua Richardson <jrichardson@wearebond.com>
2024-07-05 14:45:12 -0600

    2048-refactor (#1)
    * fix(game): more descriptive function name
    
    * chore(game): begin identifying concerns to separate
    
    * fix(addNewTile): Move logic to grid for separation of concerns
    
    * fix(grid-rendering): Separation of Concerns... move rendering of grid to Grid class
    
    * fix(game): shift all logic on how grid should be manipulated to the grid class
    
    * chore(grid): Placeholder commit for secondary refactor.
    
    * fix(game): completion of separation of concerns

--- src/pages/2048/js/game.ts
+++ src/pages/2048/js/game.ts
@@ -1,14 +1,16 @@
 import { Grid } from './grid'
 
 export class Game {
+  // render concerns
   private canvas: CanvasRenderingContext2D
   private gameElement: HTMLButtonElement
   private statusElement: HTMLElement
   private scoreElement: HTMLElement
+  //game state
   private inGame: boolean
   private gameWon: boolean
   private rowLength: number
-  private score: number
+  private _score: number
   private grid: Grid
   private gameboard: number[][] = []
 
@@ -33,15 +35,28 @@ export class Game {
     this.inGame = false
     this.gameWon = false
     this.rowLength = 4
-    this.score = 0
-    this.grid = new Grid(this.rowLength)
+    this._score = 0
+    this.grid = new Grid({
+      columnCount: this.rowLength,
+      gameReference: this,
+      rowCount: this.rowLength,
+    })
+  }
+
+  public setScore(value: number) {
+    console.log('setting score', value)
+    this._score += value
   }
 
   public newGame() {
     this.gameElement.innerText = 'Playing...'
     this.statusElement.classList.toggle('hidden', true)
-    this.grid = new Grid(this.rowLength)
-    this.gameboard = this.grid.addNew(this.grid.grid)
+    this.grid = new Grid({
+      columnCount: this.rowLength,
+      gameReference: this,
+      rowCount: this.rowLength,
+    })
+    this.grid.addNewTile()
     this.inGame = true
 
     this._updateCanvas()
@@ -53,82 +68,23 @@ export class Game {
   public move(dir: 'down' | 'left' | 'right' | 'up') {
     if (!this.inGame) return
 
-    let undoReverse = false
-    let undoTranspose = false
+    this.grid.slideTiles(dir)
 
-    switch (dir) {
-      case 'up':
-        undoReverse = true
-        this.gameboard = this.grid.reverseGrid(this.gameboard)
-        break
-      case 'right':
-        undoTranspose = true
-        this.gameboard = this.grid.transposeGrid(this.gameboard)
-        break
-      case 'left':
-        undoReverse = true
-        undoTranspose = true
-        this.gameboard = this.grid.transposeGrid(this.gameboard)
-        this.gameboard = this.grid.reverseGrid(this.gameboard)
-        break
-      default:
-        break
+    if (this.grid.isDirty) {
+      this._updateCanvas()
+      this._isGameWon()
+      this._isGameOver()
     }
-
-    const previousBoard = this.grid.makeCopy(this.gameboard)
-    for (var row in this.gameboard) {
-      this.gameboard[row] = this._operate(this.gameboard[row])
-    }
-
-    const changed = this._compare(previousBoard, this.gameboard)
-    if (undoReverse) this.gameboard = this.grid.reverseGrid(this.gameboard)
-    if (undoTranspose) this.gameboard = this.grid.transposeGrid(this.gameboard)
-    if (changed) this.gameboard = this.grid.addNew(this.gameboard)
-
-    this._updateCanvas()
-    this._isGameWon()
-    this._isGameOver()
   }
 
-  /**
-   * @description Combines the first pair filled cells in the row, and adds the value to the score
-   * @param row The array to act upon
-   */
-  private _combine(row: number[]) {
-    for (let i = this.rowLength - 1; i > 0; i--) {
-      if (row[i] !== row[i - 1] || (row[i] === 0 && row[i - 1] === 0)) continue
-
-      row[i] = row[i] + row[i - 1]
-      row[i - 1] = 0
-
-      this.score += row[i]
-    }
-
-    return row
-  }
-  /**
-   * @description Compares two grids to see if they are the same
-   * @param board1 The first grid to compare
-   * @param board2 The second grid to compare
-   * @returns True if the grids are the same, false otherwise
-   */
-  private _compare(board1: number[][], board2: number[][]) {
-    for (const row in board1) {
-      for (const cell in board1[row]) {
-        if (board1[row][cell] !== board2[row][cell]) return true
-      }
-    }
-
-    return false
-  }
   /**
    * Update the DOM when the game is ended
    */
   private _gameDone() {
     this.statusElement.innerText = this.gameWon
-      ? `Congratulations! You won the game with a score of: ${this.score}`
+      ? `Congratulations! You won the game with a score of: ${this._score}`
       : 'Better luck next time'
-    this.score = 0
+    this._score = 0
 
     this.statusElement.classList.toggle('hidden', false)
     this.gameElement.innerText = 'Play Again?'
@@ -138,95 +94,17 @@ export class Game {
    * Checks to see if the game has been won
    */
   private _isGameWon() {
-    for (const row in this.gameboard) {
-      for (const cell in this.gameboard[row]) {
-        if (this.gameboard[row][cell] === 2048) this.gameWon = true
-      }
+    if (this.grid.highestValue === 2048) {
+      this.gameWon = true
     }
   }
   /**
    * Checks to see if the gameboard has any moves left
    */
   private _isGameOver() {
-    let isGameOver = true,
-      cellValue = -1,
-      isLastRow = false,
-      isLastCol = false,
-      nextRowValue = -1,
-      nextColValue = -1
-
-    cellCheck: for (let row = 0; row < this.rowLength; row++) {
-      for (let col = 0; col < this.gameboard[row].length; col++) {
-        cellValue = this.gameboard[row][col]
-        isLastRow = row === this.rowLength - 1
-        isLastCol = col === this.gameboard[row].length - 1
-        nextRowValue = isLastRow ? -1 : this.gameboard[row + 1][col]
-        nextColValue = isLastCol ? -1 : this.gameboard[row][col + 1]
-
-        if (
-          cellValue === 0 ||
-          (!isLastRow && cellValue === nextRowValue) ||
-          (!isLastCol && cellValue === nextColValue)
-        ) {
-          isGameOver = false
-          break cellCheck
-        }
-      }
-    }
-
-    if (isGameOver) {
-      console.log('game over')
+    if (!this.grid.hasMovesToMake) {
       this.inGame = false
       this._gameDone()
-    }
-  }
-  /**
-   * @description Shifts all filled cells to the right, and combines the first pair of numbers
-   * @param row The gameboard row to operate on
-   * @returns
-   */
-  private _operate(row: number[]) {
-    row = this._slide(row)
-    row = this._combine(row)
-    row = this._slide(row)
-    return row
-  }
-  /**
-   * @description Slides all filled cells to the right
-   * @param row The array to act upon
-   * @returns
-   */
-  private _slide(row: number[]): number[] {
-    let slidRow = row.filter(Boolean)
-
-    return [...Array(this.rowLength - slidRow.length).fill(0), ...slidRow]
-  }
-
-  /**
-   * @description Draws the gameboard to the canvas
-   */
-  private _drawGrid() {
-    let w = 100
-    for (let row = 0; row < this.gameboard.length; row++) {
-      for (let col = 0; col < this.gameboard[row].length; col++) {
-        const cellValue = this.gameboard[row][col]
-        this.canvas.fillStyle = 'transparent'
-        this.canvas.lineWidth = 2
-        this.canvas.strokeStyle = '#fff'
-        this.canvas.rect(row * w, col * w, w, w)
-        this.canvas.stroke()
-
-        if (cellValue > 0) {
-          this.canvas.textAlign = 'center'
-          this.canvas.fillStyle = '#fff'
-          this.canvas.font = 'bold 2rem sans-serif'
-          this.canvas.fillText(
-            cellValue.toString(),
-            row * w + w / 2,
-            col * w + w / 2
-          )
-        }
-      }
     }
   }
 
@@ -248,7 +126,7 @@ export class Game {
       this.canvas.canvas.height
     )
 
-    this._drawGrid()
-    this.scoreElement.innerText = this.score.toString()
+    this.grid.render(this.canvas)
+    this.scoreElement.innerText = this._score.toString()
   }
 }
--- src/pages/2048/js/grid.ts
+++ src/pages/2048/js/grid.ts
@@ -1,59 +1,279 @@
+import type { Game } from './game'
+
 export class Grid {
-  public grid: number[][]
+  // initialization concerns
+  /** The number of rows in the grid */
+  private _rowCount: number
+  /** The number of columns in the grid */
+  private _columnCount: number
+  /** The game instance */
+  private _game: Game
+  /** An array that maps the grid to its transposition */
+  private _transpositionMap: number[]
 
-  constructor(rowLength: number) {
-    this.grid = new Array(rowLength)
+  // grid state
+  /** The grid data */
+  private _grid: number[]
+  /** Whether the grid has been modified */
+  private _isDirty: boolean
+  /** Whether the grid has any valid moves remaining */
+  private _hasMovesToMake: boolean
+  /** The highest value in the grid */
+  private _highestValue: number
+
+  constructor({
+    rowCount,
+    columnCount,
+    gameReference,
+  }: {
+    rowCount: number
+    columnCount: number
+    gameReference: Game
+  }) {
+    this._rowCount = rowCount
+    this._columnCount = columnCount
+    this._game = gameReference
+
+    this._isDirty = false
+    this._hasMovesToMake = true
+    this._highestValue = 0
+    this._grid = new Array(rowCount * columnCount).fill(0)
+    this._transpositionMap = this._createTranspositionMap(rowCount, columnCount)
+  }
+
+  public get grid() {
+    return this._grid
+  }
+  public get isDirty() {
+    return this._isDirty
+  }
+  public get hasMovesToMake() {
+    return this._hasMovesToMake
+  }
+  public get highestValue() {
+    return this._highestValue
+  }
+
+  /**
+   * @description Adds a new tile to a random position in the grid
+   */
+  public addNewTile() {
+    const cellsAvailable = this._grid
+      .map((value, index) => (value === 0 ? index : null))
+      .filter(Boolean)
+
+    if (cellsAvailable.length === 0) return
+
+    const randomCell =
+      cellsAvailable[Math.floor(Math.random() * cellsAvailable.length)]
+
+    if (randomCell && randomCell < this._grid.length) {
+      this._grid[randomCell] = Math.random() > 0.1 ? 2 : 4
+    }
+  }
+  /**
+   * @description Creates a shallow clone of the grid
+   * @returns A copy of the grid
+   */
+  public makeCopy() {
+    const newGrid = new Grid({
+      columnCount: this._columnCount,
+      gameReference: this._game,
+      rowCount: this._rowCount,
+    })
+    for (let index = 0; index < this._grid.length; index++) {
+      newGrid.updateCell({ index, value: this._grid[index] })
+    }
+
+    return newGrid.grid
+  }
+  /**
+   * @description Draws the gameboard to the canvas
+   * @param renderer The canvas context to draw to
+   */
+  public render(renderer: CanvasRenderingContext2D) {
+    const cellWidth = renderer.canvas.width / this._columnCount
+    const cellHeight = renderer.canvas.height / this._rowCount
+
+    for (let index = 0; index < this._grid.length; index++) {
+      const cellValue = this._grid[index]
+      const xPosition = (index % this._columnCount) * cellWidth
+      const yPosition = Math.floor(index / this._columnCount) * cellHeight
+
+      // render the cell's border
+      renderer.fillStyle = 'transparent'
+      renderer.lineWidth = 2
+      renderer.strokeStyle = '#fff'
+      renderer.rect(xPosition, yPosition, cellWidth, cellHeight)
+      renderer.stroke()
+
+      // render the cell's value
+      if (cellValue > 0) {
+        renderer.textAlign = 'center'
+        renderer.fillStyle = '#fff'
+        renderer.font = 'bold 2rem sans-serif'
+        renderer.fillText(
+          cellValue.toString(),
+          xPosition + cellWidth / 2,
+          yPosition + cellHeight / 2
+        )
+      }
+    }
+
+    this._isDirty = false
+  }
+  /**
+   * @description Slides the tiles in a given direction
+   * @param direction The direction to slide the tiles in
+   */
+  public slideTiles(direction: 'down' | 'left' | 'right' | 'up') {
+    let undoReverse = false
+    let undoTranspose = false
+
+    switch (direction) {
+      case 'up':
+        undoTranspose = true
+        undoReverse = true
+        this._transposeGrid()
+        this._reverseGrid()
+        break
+      case 'down':
+        undoTranspose = true
+        this._transposeGrid()
+        break
+      case 'left':
+        undoReverse = true
+        this._reverseGrid()
+        break
+      default:
+        break
+    }
+    console.log(
+      'undo reverse: ',
+      undoReverse,
+      'undo transpose: ',
+      undoTranspose
+    )
+    const previousBoard = this.makeCopy()
+    this._collapse()
+    const changed = this._detectChange(previousBoard, this._grid)
+    if (undoReverse) this._reverseGrid()
+    if (undoTranspose) this._transposeGrid()
+    if (changed) {
+      this._highestValue = this._grid.reduce((a, b) => Math.max(a, b), 0)
+      this.addNewTile()
+      this._isDirty = true
+    }
+  }
+  /**
+   * @description Updates the value of a cell in the grid
+   * @param record A record containing the index of the cell to update, and the value to set it to
+   */
+  public updateCell({ index, value }: { index: number; value: number }) {
+    this._grid[index] = value
+  }
+
+  /**
+   * @description Shifts all filled cells to the right, and combines pairs within the row
+   */
+  private _collapse() {
+    let gridRows: Array<Array<(typeof this._grid)[number]>> = []
+
+    for (
+      let row = 0;
+      row < this._columnCount * this._rowCount;
+      row += this._columnCount
+    ) {
+      gridRows.push(this._grid.slice(row, row + this._columnCount))
+    }
+    for (const row in gridRows) {
+      gridRows[row] = this._slide(this._combine(this._slide(gridRows[row])))
+    }
+    this._grid = gridRows.flat()
+  }
+
+  /**
+   * @description Compares two grids to see if they are different
+   * @param board1 A grid to compare against
+   * @param board2 A grid to compare against
+   * @returns Whether the grids are different
+   */
+  private _detectChange(board1: typeof this._grid, board2: typeof this._grid) {
+    return !board1.every((value, index) => value === board2[index])
+  }
+  /**
+   * @description Reverses the values of the rows in the grid
+   */
+  private _reverseGrid() {
+    console.log('reverse grid')
+    let index = 0,
+      tempValue = -1
+    while (index < this._grid.length) {
+      if (index % this._columnCount === 0) {
+        tempValue = this._grid[index]
+        this._grid[index] = this._grid[index + this._columnCount - 1]
+        this._grid[index + this._columnCount - 1] = tempValue
+        index++
+      } else {
+        tempValue = this._grid[index]
+        this._grid[index] = this._grid[index + 1]
+        this._grid[index + 1] = tempValue
+        index += this._columnCount - 1
+      }
+    }
+  }
+  /**
+   * @description Transposes the values in the grid
+   */
+  private _transposeGrid() {
+    this._grid = this._transpositionMap.map(index => this._grid[index])
+  }
+  /**
+   * @description Slides all filled cells to the right
+   * @param row The array to act upon
+   * @returns
+   */
+  private _slide(
+    row: Array<(typeof this._grid)[number]>
+  ): Array<(typeof this._grid)[number]> {
+    let slidRow = row.filter(Boolean)
+
+    return [...Array(this._columnCount - slidRow.length).fill(0), ...slidRow]
+  }
+  /**
+   * @description Combines the first pair filled cells in the row, and adds the value to the score
+   * @param row The array to act upon
+   */
+  private _combine(row: number[]) {
+    console.log('combining row', row)
+    for (let i = this._columnCount - 1; i > 0; i--) {
+      if (row[i] !== row[i - 1]) continue
+
+      console.log(
+        `combining ${row[i]} and ${row[i - 1]} = ${row[i] + row[i - 1]}`
+      )
+      row[i] = row[i] + row[i - 1]
+      row[i - 1] = 0
+
+      console.log('value', row[i], typeof row[i])
+      if (row[i]) this._game.setScore(row[i])
+    }
+
+    return row
+  }
+  private _createTranspositionMap(rowCount: number, columnCount: number) {
+    console.log('creating map', rowCount, columnCount)
+    const map = new Array(rowCount * columnCount)
       .fill(0)
-      .map(() => new Array(rowLength).fill(0))
-  }
+      .map((_, index) => index)
+    const output: number[] = []
 
-  public addNew(grid: number[][]) {
-    const options: { x: number; y: number }[] = []
-
-    for (let row = 0; row < grid.length; row++) {
-      for (let cell = 0; cell < grid[row].length; cell++) {
-        if (grid[row][cell] === 0) {
-          options.push({ x: row, y: cell })
-        }
+    for (let row = 0; row < rowCount; row++) {
+      for (let col = 0; col < columnCount; col++) {
+        output[col * rowCount + row] = map[row * columnCount + col]
+        output[row * columnCount + col] = map[col * rowCount + row]
       }
     }
-
-    if (options.length === 0) return grid
-
-    const spot = options[Math.floor(Math.random() * options.length)]
-    grid[spot.x][spot.y] = Math.random() > 0.1 ? 2 : 4
-
-    return grid
-  }
-
-  public makeCopy(grid: number[][]) {
-    const newGrid = new Grid(grid[0].length)
-    const copyGrid = newGrid.grid
-
-    for (let row = 0; row < grid.length; row++) {
-      for (let cell = 0; cell < grid[row].length; cell++) {
-        copyGrid[row][cell] = grid[row][cell]
-      }
-    }
-
-    return copyGrid
-  }
-  public reverseGrid(grid: number[][]) {
-    for (var row in grid) {
-      grid[row].reverse()
-    }
-    return grid
-  }
-  public transposeGrid(grid: number[][]) {
-    const newGrid = new Grid(grid[0].length)
-    const transposedGrid = newGrid.grid
-
-    for (let row = 0; row < grid.length; row++) {
-      for (let col = 0; col < grid[row].length; col++) {
-        transposedGrid[row][col] = grid[col][row]
-      }
-    }
-
-    return transposedGrid
+    return output
   }
 }
