19f5b7ee8421121241b130706b5e36c8c892e6cc
Joshua Richardson <jrichardson@wearebond.com>
2024-07-12 11:50:11 -0600

    chore(organization): move constants to their own file

---
+++ src/pages/2048/js/constants.ts
@@ -0,0 +1,10 @@
+/** A percentage based value for the spacing between the cells based on the size of the spacing in the figma file */
+export const BOARD_SPACING = 3 / 95
+
+export const colors = {
+  LATEST: '#0000ff',
+  HIGHEST: '#00ff0025',
+  EMPTY: '#eeeeee25',
+  FILLED: '#eee',
+  TEXT: '#000',
+}
